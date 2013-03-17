6a8c40b34abf56a32396a955aa8b1f13480ec5d7
Andrew Bradley <cspotcode@gmail.com>
2013-03-17 12:42:38 -0400

    Cleaned up build script by using ShellJS: Node implementations of common unix shell commands

--- build.js
+++ build.js
@@ -1,64 +1,39 @@
-var fs = require('fs');
-var path = require('path');
 var jade = require('jade');
 var stylus = require('stylus');
 var _ = require('lodash');
+require('shelljs/global');
 
-// create output directory
-createDirectory('out');
-createDirectory('out/demo');
-createDirectory('out/demo/vendor');
+var input, output;
+
+// create output directories
+mkdir('-p', 'out/demo/vendor');
 
 // render demo html
-var input = readFile('demo/index.jade');
-var output = jade.compile(input)();
-writeFile('out/demo/index.html', output);
+input = cat('demo/index.jade');
+output = jade.compile(input)();
+output.to('out/demo/index.html');
 
 // render demo css
-var input = readFile('demo/style.styl');
+input = cat('demo/style.styl');
 stylus(input).render(function(err, output) {
     if(err) throw err;
-    writeFile('out/demo/style.css', output);
+    output.to('out/demo/style.css');
 });
 
-// copy scripts
-_.each([
-    'demo/vendor/jquery-1.9.1.min.js',
-    'demo/vendor/knockout-2.2.1.min.js'
-    ], function(v) {
-    copyFile(v, path.join('out', v));
-});
-
-copyFile('node_modules/lodash/dist/lodash.min.js', 'out/demo/vendor/lodash.min.js');
+// copy third-party scripts
+cp('-f', 'demo/vendor/*', 'out/demo/vendor/');
+cp('-f', 'node_modules/lodash/dist/lodash.min.js', 'out/demo/vendor/lodash.min.js');
 
 // copy angl AST-generator to demo
-writeFile('out/demo/vendor/angl.js', wrapForBrowser(readFile('node_modules/angl/out/angl.js')));
-writeFile('out/demo/vendor/parser.js', wrapForBrowser(readFile('node_modules/angl/out/parser.js')));
+wrapForBrowser(cat('node_modules/angl/out/angl.js')).to('out/demo/vendor/angl.js');
+wrapForBrowser(cat('node_modules/angl/out/parser.js')).to('out/demo/vendor/parser.js');
 
 // copy compiler to demo
-writeFile('out/demo/main.js', wrapForBrowser(readFile('lib/main.js')));
+wrapForBrowser(cat('lib/main.js')).to('out/demo/main.js');
 
 ////
 // UTILITY FUNCTIONS
 ////
-
-function createDirectory(dir) {
-    try {
-        fs.mkdirSync(path.join(__dirname, dir));
-    } catch(e) {}
-}
-
-function readFile(file) {
-    return fs.readFileSync(path.join(__dirname, file)).toString();
-}
-
-function writeFile(file, content) {
-    fs.writeFileSync(path.join(__dirname, file), content);
-}
-
-function copyFile(inFile, outFile) {
-    writeFile(outFile, readFile(inFile));
-}
 
 function wrapForBrowser(input) {
     return ';(function(module, exports){\n' + input + '\n})(window.module, window.module.exports);';
