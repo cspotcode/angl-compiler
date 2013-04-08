bf1ec111608a5be1cb5c95af003c79c2987416a0
Andrew Bradley <cspotcode@gmail.com>
2013-04-08 10:05:04 -0400

    scriptdefs, consts, and vars are registered into scope and reject duplicate names.

--- lib/process-phase-one.ts
+++ lib/process-phase-one.ts
@@ -5,23 +5,52 @@
 import treeWalker = module('./tree-walker');
 import scope = module('./angl-scope');
 import astTypes = module('./ast-types');
+var _ = require('lodash');
 var walk = treeWalker.walk;
 
 // Create scopes for all nodes
 
 export var transform = (ast:astTypes.AstNode) => {
-    walk(ast, (node:astTypes.AstNode, parent:astTypes.AstNode, locationInParent) => {
+    // TODO fix the typing on node.  I don't know how to access arbitrary properties of an object implementing an interface.
+    walk(ast, (node:any, parent:astTypes.AstNode, locationInParent) => {
         node.parentNode = parent;
         if(parent) {
             node.globalAnglScope = parent.globalAnglScope;
             node.anglScope = parent.anglScope;
         }
 
+        // TODO convert all scriptdefs into consts
+        // Will be a good test of replacing nodes
+
+        // Script definitions register an identifier into the parent scope
+        if(node.type === 'scriptdef' || node.type === 'const') {
+            if(node.parentNode.type !== 'file') {
+                throw new Error(node.type + ' must be at the root level of a file.');
+            }
+            node.anglScope.addIdentifier(node.name, node.type);
+        }
+
+        // Const definitions register an identifier into the parent scope
+
         // Scripts create a new scope
         if(node.type === 'script' || node.type === 'scriptdef') {
             var newScope = new scope.AnglScope();
             newScope.setParentScope(node.anglScope);
             node.anglScope = newScope;
+            // Register script arguments into the local scope
+            // TODO how to handle self and other?
+            _.each(node.args, (argName) => {
+                newScope.addIdentifier(argName, 'argument');
+            });
         }
+
+        // Var declarations register local variables into their scope
+        if(node.type === 'var_item') {
+            if(node.anglScope.hasIdentifier(node.name)) {
+                throw new Error('Attempt to declare local variable with the name ' + JSON.stringify(node.name) + ' more than once.');
+            }
+            node.anglScope.addIdentifier(node.name, 'localvar');
+        }
+
     });
 }
