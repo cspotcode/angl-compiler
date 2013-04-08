4c9a06c57f2502f9e14267ebdeba3b658e66a3d7
Andrew Bradley <cspotcode@gmail.com>
2013-04-08 09:53:47 -0400

    Changed AST phase zero to replace the root "statements" node with a "file" node, rather than nesting the "statements" node inside the "file" node

--- lib/process-phase-zero.ts
+++ lib/process-phase-zero.ts
@@ -3,13 +3,18 @@ var globalScope = require('./global-scope');
 import AnglScope = module('./angl-scope');
 
 // Wrap the entire AST in a "file" node
-export var transform = (ast:types.AstNode):types.AstNode => {
+// TODO fix typing of ast argument
+export var transform = (ast:any):types.AstNode => {
     var anglScope = new AnglScope.AnglScope();
     var globalAnglScope = globalScope.createGlobalScope();
     anglScope.setParentScope(globalAnglScope);
+    // Verify that the root node is of type "statements"
+    if(ast.type !== 'statements') {
+        throw new Error('Unexpected root node from Angl parser. Expected type "statements", got "' + ast.type + '".');
+    }
     return <types.AstNode>{
         type: "file",
-        stmts: ast,
+        stmts: ast.list,
         globalAnglScope: globalAnglScope,
         anglScope: anglScope
     }
