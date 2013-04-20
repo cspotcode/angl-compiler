7c4841ed790a2bdb5911a2bf162c9a28b15dd54d
Andrew Bradley <cspotcode@gmail.com>
2013-04-20 01:19:22 -0400

    Updates for parser's ability to track source locations, comments, and whitespace in the generated AST
    - parser generates root "file" node, doesn't require us to create it
      anymore

--- lib/process-phase-zero.ts
+++ lib/process-phase-zero.ts
@@ -2,20 +2,17 @@ import types = module('./ast-types');
 var globalScope = require('./global-scope');
 import AnglScope = module('./angl-scope');
 
-// Wrap the entire AST in a "file" node
+// Create global and file scopes
 // TODO fix typing of ast argument
 export var transform = (ast:any):types.AstNode => {
     var anglScope = new AnglScope.AnglScope();
     var globalAnglScope = globalScope.createGlobalScope();
     anglScope.setParentScope(globalAnglScope);
     // Verify that the root node is of type "statements"
-    if(ast.type !== 'statements') {
-        throw new Error('Unexpected root node from Angl parser. Expected type "statements", got "' + ast.type + '".');
+    if(ast.type !== 'file') {
+        throw new Error('Unexpected root node from Angl parser. Expected type "file", got "' + ast.type + '".');
     }
-    return <types.AstNode>{
-        type: "file",
-        stmts: ast.list,
-        globalAnglScope: globalAnglScope,
-        anglScope: anglScope
-    }
+    ast.anglScope = anglScope;
+    ast.globalScope = globalAnglScope;
+    return ast;
 };
