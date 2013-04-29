be8144b6b474cd72494e0ad37e509422876f688c
Andrew Bradley <cspotcode@gmail.com>
2013-04-28 20:54:14 -0400

    Updated AST transformations to use new AnglScope and Variable logic
    - properly handles local variable allocation
    - creates "self" and "other" identifiers in script scopes
    - sets scriptdefs and consts to be on the "anglGlobals" object
    - no longer sets nodes' parentNode, instead relying on the AST walker

--- lib/process-phase-one.ts
+++ lib/process-phase-one.ts
@@ -5,6 +5,8 @@
 import treeWalker = module('./tree-walker');
 import scope = module('./angl-scope');
 import astTypes = module('./ast-types');
+import astUtils = module('./ast-utils');
+import scopeVariable = module('./Variable');
 var _ = require('lodash');
 var walk = treeWalker.walk;
 
@@ -13,11 +15,8 @@ var walk = treeWalker.walk;
 export var transform = (ast:astTypes.AstNode) => {
     // TODO fix the typing on node.  I don't know how to access arbitrary properties of an object implementing an interface.
     walk(ast, (node:any, parent:astTypes.AstNode, locationInParent) => {
-        node.parentNode = parent;
-        if(parent) {
-            node.globalAnglScope = parent.globalAnglScope;
-            node.anglScope = parent.anglScope;
-        }
+
+        var replacement:any[];
 
         // TODO convert all scriptdefs into consts
         // Will be a good test of replacing nodes
@@ -27,7 +26,9 @@ export var transform = (ast:astTypes.AstNode) => {
             if(node.parentNode.type !== 'file') {
                 throw new Error(node.type + ' must be at the root level of a file.');
             }
-            node.anglScope.addIdentifier(node.name, node.type);
+            var globalVar = new scopeVariable.Variable(node.name, 'PROP_ASSIGNMENT', 'PROP_ACCESS');
+            globalVar.setContainingObjectIdentifier('anglGlobals');
+            astUtils.getGlobalAnglScope(node).addVariable(globalVar);
         }
 
         // Const definitions register an identifier into the parent scope
@@ -35,21 +36,42 @@ export var transform = (ast:astTypes.AstNode) => {
         // Scripts create a new scope
         if(node.type === 'script' || node.type === 'scriptdef') {
             var newScope = new scope.AnglScope();
-            newScope.setParentScope(node.anglScope);
+            newScope.setParentScope(astUtils.getAnglScope(node));
             node.anglScope = newScope;
             // Register script arguments into the local scope
             // TODO how to handle self and other?
+            var thisVar = new scopeVariable.Variable('self', 'ARGUMENT');
+            thisVar.setJsIdentifier('this');
+            newScope.addVariable(thisVar);
+            var otherVar = new scopeVariable.Variable('other', 'ARGUMENT');
+            newScope.addVariable(otherVar);
             _.each(node.args, (argName) => {
-                newScope.addIdentifier(argName, 'argument');
+                var argumentVar = new scopeVariable.Variable(argName, 'ARGUMENT');
+                newScope.addVariable(argumentVar);
             });
         }
 
         // Var declarations register local variables into their scope
-        if(node.type === 'var_item') {
-            if(node.anglScope.hasIdentifier(node.name)) {
-                throw new Error('Attempt to declare local variable with the name ' + JSON.stringify(node.name) + ' more than once.');
-            }
-            node.anglScope.addIdentifier(node.name, 'localvar');
+        if(node.type === 'var') {
+            replacement = [];
+            _.each(node.list, (var_item) => {
+                if(astUtils.getAnglScope(node).hasIdentifier(var_item.name)) {
+                    throw new Error('Attempt to declare local variable with the name ' + JSON.stringify(var_item.name) + ' more than once.');
+                }
+                var localVar = new scopeVariable.Variable(var_item.name);
+                astUtils.getAnglScope(node).addVariable(localVar);
+                if(var_item.expr) {
+                    replacement.push({
+                        type: 'assign',
+                        lval: {
+                            type: 'identifier',
+                            variable: localVar
+                        },
+                        rval: var_item.expr
+                    });
+                }
+            });
+            return replacement;
         }
 
     });
