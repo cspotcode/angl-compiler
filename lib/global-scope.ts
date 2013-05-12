f588c9e20e924a2d34d069117c03d6cf84c91a6c
Andrew Bradley <cspotcode@gmail.com>
2013-05-12 16:39:23 -0400

    Added "true" and "false" to global scope

--- lib/global-scope.ts
+++ lib/global-scope.ts
@@ -1,14 +1,21 @@
 import scope = module('./angl-scope');
 import scopeVariable = module('./scope-variable');
 import strings = module('./strings');
+var _ = require('lodash');
 
 export function createGlobalScope():scope.AnglScope {
     var globalScope = new scope.AnglScope();
 
-    // TODO what values should I be adding?  Gotta invent an object/type/schema for values.
-    var variable = new scopeVariable.Variable('global', 'PROP_ASSIGNMENT', 'PROP_ACCESS');
-    variable.setContainingObjectIdentifier(strings.ANGL_GLOBALS_IDENTIFIER);
-    globalScope.addVariable(variable);
+    var globalIdentifiers:any = 'global true false';
+
+    // Add all global identifiers into global scope
+    globalIdentifiers = globalIdentifiers.split(' ');
+    _.each(globalIdentifiers, (globalIdentifier) => {
+        // TODO what values should I be adding?  Gotta invent an object/type/schema for values.
+        var variable = new scopeVariable.Variable(globalIdentifier, 'PROP_ASSIGNMENT', 'PROP_ACCESS');
+        variable.setContainingObjectIdentifier(strings.ANGL_GLOBALS_IDENTIFIER);
+        globalScope.addVariable(variable);
+    });
 
     return globalScope;
 };
