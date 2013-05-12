0ec158aed076ff369229b336b05ade45ffeb372a
Andrew Bradley <cspotcode@gmail.com>
2013-05-12 02:05:22 -0400

    Outputted code is now a RequireJS module

--- lib/strings.ts
+++ lib/strings.ts
@@ -1,2 +1,4 @@
-export var ANGL_GLOBALS_IDENTIFIER = "$AG";
-export var ANGL_RUNTIME_IDENTIFIER = "$ART";
+export var ANGL_GLOBALS_IDENTIFIER = '$AG';
+export var ANGL_GLOBALS_MODULE = 'AnglGlobals';
+export var ANGL_RUNTIME_IDENTIFIER = '$ART';
+export var ANGL_RUNTIME_MODULE = 'AnglRuntime';
