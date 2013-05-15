fe2bef4df8e98c488ffbbb07caef4dc51095aacd
Andrew Bradley <cspotcode@gmail.com>
2013-05-14 20:51:44 -0400

    Added prefix for RequireJS module names of compile Angl files
    - I imagine that when it's time to combine all compiled angl files
      together, we might use requirejs to do that
    - this prefix can be used in whatever script is generated to combine them
      all together

--- lib/strings.ts
+++ lib/strings.ts
@@ -4,5 +4,7 @@ export var ANGL_GLOBALS_MODULE = 'AnglGlobals';
 // variable name and RequireJS module name for the Angl runtime
 export var ANGL_RUNTIME_IDENTIFIER = '$ART';
 export var ANGL_RUNTIME_MODULE = 'AnglRuntime';
+// Prefix for the RequireJS module name of a compiled Angl file
+export var ANGL_FILE_MODULE_PREFIX = 'CompiledAngl/';
 // Name of the default super-object of all Angl objects
 export var SUPER_OBJECT_NAME = 'AnglObject';
